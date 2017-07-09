5dedc172ceb93ffae6e6f100b8903f29811d9b2d
andy salerno <andysalerno@gmail.com>
2017-07-09 00:20:19 -0700

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,1 @@
-mod tree;
-use tree::{Tree, TreeNode};
-
-struct TestStruct;
-
-fn buildit() {
-    let mut tree = Tree::new();
-
-    tree.set_root(TestStruct{});
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+pub mod tree;
--- src/tree.rs
+++ src/tree.rs
@@ -1,18 +1,14 @@
-use std::rc::Weak;
-
 pub struct TreeNode<T> {
     data: T,
-    parent: Option<Weak<TreeNode<T>>>, 
     children: Vec<Box<TreeNode<T>>>,
 }
 
-impl <T> TreeNode<T> {
+impl<T> TreeNode<T> {
     pub fn new(data: T) -> Self {
-        TreeNode { data: data, parent: None, children: Vec::new() }
-    }
-
-    pub fn new_child(data: T, parent: Weak<TreeNode<T>>) -> Self {
-        TreeNode { data: data, parent: parent, children: Vec::new() }
+        TreeNode {
+            data: data,
+            children: Vec::new(),
+        }
     }
 
     pub fn get_children(&mut self) -> &mut Vec<Box<TreeNode<T>>> {
@@ -21,15 +17,15 @@ impl <T> TreeNode<T> {
 }
 
 pub struct Tree<T> {
-    root: Option<Box<TreeNode<T>>>
+    root: Option<Box<TreeNode<T>>>,
 }
 
-impl <T> Tree<T> {
+impl<T> Tree<T> {
     pub fn new() -> Self {
-        Tree{ root: None }
+        Tree { root: None }
     }
 
-    pub fn set_root(&mut self, data: T) -> &TreeNode<T> {
+    pub fn add_root(&mut self, data: T) -> &TreeNode<T> {
         let root_node = TreeNode::new(data);
         self.root = Some(Box::new(root_node));
 
@@ -37,8 +33,7 @@ impl <T> Tree<T> {
     }
 
     pub fn add_child(parent: &mut TreeNode<T>, data: T) -> &TreeNode<T> {
-        let parent_ptr = 
-        let child_node = TreeNode::new_child(data);
+        let child_node = TreeNode::new(data);
         {
             let children = &mut parent.get_children();
             children.push(Box::new(child_node));
